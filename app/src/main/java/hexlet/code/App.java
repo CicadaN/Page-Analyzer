04090e08b9da246feb43a10dcde1d024195e9ef0
cicad <cicadac@ya.ru>
2024-10-08 22:43:12 +0300

    Fix deploy #3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,21 +1,38 @@
 package hexlet.code;
 
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import hexlet.code.repository.DatabaseConfig;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class App {
+    public static Javalin getApp() throws IOException, SQLException {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(getDatabaseUrl());
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+        String sql = readResourceFile("schema.sql");
 
-    private static HikariDataSource dataSource;
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
 
-    public static Javalin getApp() {
-        dataSource = DatabaseConfig.getDataSource();
+        BaseRepository.dataSource = dataSource;
 
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
@@ -27,25 +44,30 @@ public class App {
         });
 
         app.get("/", ctx -> ctx.result("Hello World"));
-
         return app;
+    }
+
+    private static String readResourceFile(String fileName) throws IOException {
+        InputStream inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return bufferedReader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+
+    private static String getDatabaseUrl() {
+        return System.getenv()
+                .getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+
     }
 
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
-        return Integer.parseInt(port);
+        return Integer.valueOf(port);
     }
 
     public static void main(String[] args) throws IOException, SQLException {
-        var app = getApp();
+        Javalin app = getApp();
         app.start(getPort());
-        try (var connection = dataSource.getConnection();
-             var statement = connection.createStatement()) {
-            statement.execute("CREATE TABLE IF NOT EXISTS urls ("
-                    + "id BIGINT AUTO_INCREMENT PRIMARY KEY,"
-                    + "name VARCHAR(255) NOT NULL,"
-                    + "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
-                    + ")");
-        }
     }
 }
--- app/src/main/java/hexlet/code/repository/BaseRepository.java
+++ app/src/main/java/hexlet/code/repository/BaseRepository.java
@@ -2,11 +2,7 @@ package hexlet.code.repository;
 
 import com.zaxxer.hikari.HikariDataSource;
 
-public abstract class BaseRepository {
-    protected HikariDataSource dataSource;
-
-    public BaseRepository(HikariDataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+public class BaseRepository {
+    public static HikariDataSource dataSource;
 }
 
